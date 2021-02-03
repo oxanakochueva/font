9aa12d02bc787011cc3a9f7fa4e28e047a65e62f
ZakharDay <zakhar.day@gmail.com>
2021-02-03 12:19:45 +0300

    Create font_library.js

---
+++ src/app/font_library.js
@@ -0,0 +1,72 @@
+const fonts = {
+  josefin: {
+    heading: 'Josefin Sans',
+    texts: {
+      en: {
+        description: "<p>Bla bla bla</p><p>To contribute <a href='http://yo.rap'>http://yo.rap</a></p>",
+        designer: {
+          en: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          },
+          ru: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          }
+        }
+      },
+      ru: {
+        description: "<p>Bla bla bla</p><p>To contribute <a href='http://yo.rap'>http://yo.rap</a></p>",
+        designer: {
+          en: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          },
+          ru: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          }
+        }
+      }
+    }
+  },
+  cardo: {
+    heading: 'Cardo',
+    texts: {
+      en: {
+        description: "<p>Bla bla bla</p><p>To contribute <a href='http://yo.rap'>http://yo.rap</a></p>",
+        designer: {
+          en: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          }
+        }
+      },
+      ru: {
+        description: "<p>Bla bla bla</p><p>To contribute <a href='http://yo.rap'>http://yo.rap</a></p>",
+        designer: {
+          en: {
+            name: '',
+            company: '',
+            userpic: 'http://fonts.google.com/userpic.png'
+          }
+        }
+      }
+    }
+  },
+}
+
+const pairs = {
+  josefin_cardo: {
+    heading: 'Josefing Sans + Cardo',
+    fonts: ['josefin', 'cardo'],
+    languages: ['en']
+  }
+}
+
+export {fonts, pairs}
